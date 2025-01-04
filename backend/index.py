8523cafb7e6356f364e3445816dd31136212634b
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-04 18:58:41 +0900

    fix

--- backend/index.py
+++ backend/index.py
@@ -237,10 +237,10 @@ if __name__ == "__main__":
     server = FastApiServer(allow_origins=["https://yuta-air.local:3000"])
     socket_server = SocketServer(allow_origins=["https://yuta-air.local:3000"])
     socket_server.set_handlers(server.app)
-    web_rtc_server = WebRtcServer()
-    web_rtc_server.set_handlers(socket_server.sio)
-    vision_processor = VisionProcessor()
-    web_rtc_server.set_on_frame_received(vision_processor.on_frame_received)
+    # web_rtc_server = WebRtcServer()
+    # web_rtc_server.set_handlers(socket_server.sio)
+    # vision_processor = VisionProcessor()
+    # web_rtc_server.set_on_frame_received(vision_processor.on_frame_received)
 
     import uvicorn
     uvicorn.run(server.app,
