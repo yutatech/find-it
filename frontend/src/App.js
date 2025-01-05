e61c3161892d344ceb817bb5e459074b698a9bb1
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-06 01:09:20 +0900

    frontend: SocketRefContextを追加、App全体に適用

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,8 @@ import Logo from "./component/SharedComponents/Logo";
 import Header from './component/SharedComponents/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { SocketRefProvider } from './modules/SocketRefContext';
+
 function App() {
   return (
     <Router>
@@ -19,16 +21,19 @@ function App() {
         {/* 常に右上に表示されるロゴ */}
         <Header />
         <Logo />
-        {/* ルーティング設定 */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Labelselect />} />
-          <Route path="/page2/camera" element={<Camera2 />} />
-          <Route path="/page1/addphoto" element={<AddPhoto />} />
-          <Route path="/page1/labelmanagement" element={<LabelManagement />} />
-          <Route path="/page1/editphoto" element={<EditPhoto />} />
-        </Routes>
+        {/* Socket通信のインスタンスを全体で共有 */}
+        <SocketRefProvider>
+          {/* ルーティング設定 */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/page1" element={<Page1 />} />
+            <Route path="/page2" element={<Labelselect />} />
+            <Route path="/page2/camera" element={<Camera2 />} />
+            <Route path="/page1/addphoto" element={<AddPhoto />} />
+            <Route path="/page1/labelmanagement" element={<LabelManagement />} />
+            <Route path="/page1/editphoto" element={<EditPhoto />} />
+          </Routes>
+        </SocketRefProvider>
       </div>
     </Router>
 
