74d11231ea16d3a7e9d19bba6c46ef2cc007ba39
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-05 14:16:10 +0900

    fix

--- backend/index.py
+++ backend/index.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
 
-app = FastAPI()
-@app.get("/hello")
-async def root():
-    return {"message": "Hello World From Fast API"}
+# app = FastAPI()
+# @app.get("/hello")
+# async def root():
+#     return {"message": "Hello World From Fast API"}
   
+import main
+
+server = main.FastApiServer(allow_origins=["https://yuta-air.local:3000"])
+socket_server = main.SocketServer(allow_origins=["https://yuta-air.local:3000"])
+socket_server.set_handlers(server.app)
+
+app = server.app
