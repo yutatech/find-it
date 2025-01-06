250fab099f2df18cbe4b20461efd967d66c2c1a8
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-06 16:18:16 +0900

    socket通信 / webRTCを追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './component/Home/Home';
 import Page1 from './component/LearningPhase/Page1';
-import Page2 from './component/InferencePhase/Page2';
 import AddPhoto from './component/LearningPhase/TeacherData/AddPhoto';
 import LabelManagement from './component/LearningPhase/TeacherData/LabelManagement';
 import EditPhoto from './component/LearningPhase/TeacherData/EditPhoto';
@@ -14,9 +13,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { SocketRefProvider } from './modules/SocketRefContext';
 import useLocalVideo from './hooks/useLocalVideo';
+import useWebRtc from './hooks/useWebRtc';
 
-function App() {
+
+function Component() {
   const { localStreamRef, isLocalStreamReady, videoSize } = useLocalVideo();
+  const { isConnected, setupWebRtc } = useWebRtc(localStreamRef, isLocalStreamReady);
 
   return (
     <Router>
@@ -24,8 +26,6 @@ function App() {
         {/* 常に右上に表示されるロゴ */}
         <Header />
         <Logo />
-        {/* Socket通信のインスタンスを全体で共有 */}
-        <SocketRefProvider>
           {/* ルーティング設定 */}
           <Routes>
             <Route path="/" element={<Home />} />
@@ -36,10 +36,19 @@ function App() {
             <Route path="/page1/labelmanagement" element={<LabelManagement />} />
             <Route path="/page1/editphoto" element={<EditPhoto />} />
           </Routes>
-        </SocketRefProvider>
       </div>
     </Router>
+  );
+}
 
+function App() {
+  return (
+    <>
+      {/* Socket通信のインスタンスを全体で共有 */}
+      <SocketRefProvider>
+        <Component />
+      </SocketRefProvider>
+    </>
   );
 }
 
--- frontend/src/modules/SocketRefContext.js
+++ frontend/src/modules/SocketRefContext.js
@@ -9,6 +9,7 @@ export const SocketRefProvider = ({ children }) => {
   socketRef.current = io(process.env.REACT_APP_API_URL, {
     transports: ['websocket', 'polling']
   });
+  console.log('socketRef:', socketRef.current);
 
   return (
     <SocketRefContext.Provider value={socketRef}>
