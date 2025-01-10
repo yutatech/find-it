388c5f2d9c2f7e2246b956eb578b362b4822b86d
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-10 21:20:17 +0900

    backend: hostnameの取得方法を修正

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,35 @@ from modules.vision_processor import VisionProcessor
 from modules.frontend_server import FrontendServer
 from modules.database import DataBase
 import socket
-
+import subprocess
+import platform
 
 if __name__ == "__main__":
-    hostname = socket.gethostname()
+    system_name = platform.system()
+    if system_name == "Darwin":  # macOS
+        print("macOS")
+        result = subprocess.run(
+            ["scutil", "--get", "LocalHostName"], text=True, capture_output=True
+        )
+        # 結果を取得（stdoutに出力される）
+        hostname = result.stdout.strip() + ".local"
+    elif system_name == "Linux":  # Linux
+        print("Linux")
+        hostname = socket.gethostname()
+    elif system_name == "Windows":  # Windows
+        print("Windows")
+        hostname = socket.gethostname()
+    else:
+        print("Unknown OS")
+
     print(f"https://{hostname}:3000")
-    
-    server = FastApiServer(allow_origins=["https://localhost:3000", f"https://{hostname}:3000"])
-    socket_server = SocketServer(allow_origins=["https://localhost:3000", f"https://{hostname}:3000"])
+
+    server = FastApiServer(
+        allow_origins=["https://localhost:3000", f"https://{hostname}:3000"]
+    )
+    socket_server = SocketServer(
+        allow_origins=["https://localhost:3000", f"https://{hostname}:3000"]
+    )
     socket_server.set_handlers(server.app)
 
     web_rtc_server = WebRtcServer()
