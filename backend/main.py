db6c9c269ee7b416d701fd979ecc27c81b15def5
Yuta Fujiyama <yuta.technology@gmail.com>
2025-01-15 01:08:43 +0900

    main.py: get_host_name()を関数に切り出し

--- backend/main.py
+++ backend/main.py
@@ -5,14 +5,39 @@ from modules.vision_processor import VisionProcessor
 from modules.frontend_server import FrontendServer
 from modules.database import DataBase
 import socket
+import platform
+import subprocess
 
+
+def get_host_name():
+    system_name = platform.system()
+    if system_name == "Darwin":  # macOS
+        print("macOS")
+        result = subprocess.run(
+            ["scutil", "--get", "LocalHostName"], text=True, capture_output=True
+        )
+        # 結果を取得（stdoutに出力される）
+        hostname = result.stdout.strip() + ".local"
+    elif system_name == "Linux":  # Linux
+        print("Linux")
+        hostname = socket.gethostname()
+    elif system_name == "Windows":  # Windows
+        print("Windows")
+        hostname = socket.gethostname()
+    else:
+        print("Unknown OS")
+
+    print(f"https://{hostname}:3000")
+    return hostname
 
 if __name__ == "__main__":
-    hostname = socket.gethostname()
-    print(f"https://{hostname}:3000")
-    
-    server = FastApiServer(allow_origins=["https://localhost:3000", f"https://{hostname}:3000"])
-    socket_server = SocketServer(allow_origins=["https://localhost:3000", f"https://{hostname}:3000"])
+    hostname = get_host_name()
+    server = FastApiServer(
+        allow_origins=["https://localhost:3000", f"https://{hostname}:3000"]
+    )
+    socket_server = SocketServer(
+        allow_origins=["https://localhost:3000", f"https://{hostname}:3000"]
+    )
     socket_server.set_handlers(server.app)
 
     web_rtc_server = WebRtcServer()
