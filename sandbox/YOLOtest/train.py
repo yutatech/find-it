b5b23c6d496ad54b3707494a346846245e657442
kitamura <kitamura.s.ak@m.titech.ac.jp>
2025-01-20 12:46:15 +0900

    追加学習

--- sandbox/YOLOtest/train.py
+++ sandbox/YOLOtest/train.py
@@ -1,17 +1,19 @@
 from ultralytics import YOLO
 
 # 元のYOLOモデルをロード（事前学習済みのモデル）
-base_model = YOLO('/home/shingo/find-it/sandbox/YOLOtest/yolov8n.pt')  # デフォルトYOLOモデル (例: YOLOv8 Nano)
+base_model = YOLO('/home/shingo/find-it/sandbox/YOLOtest/runs/detect/yolov8n_finetuned5/weights/last.pt')  # デフォルトYOLOモデル (例: YOLOv8 Nano)
 
 # 新しいデータセットで追加学習
 results = base_model.train(
-    data= r'/mnt/c/Users/shingo/YOLO/dataset/data.yaml',  # 新しいデータセットのdata.yaml
-    epochs=50,  # 追加学習するエポック数
+    data= r'/mnt/c/Users/shingo/YOLO/watch2/data.yaml',  # 新しいデータセットのdata.yaml
+    epochs=10,  # 追加学習するエポック数
+    lr0=0.001,
+    batch=16,
+    augment=True,
     imgsz=640,  # 画像サイズ
     device='cpu',  # 'cpu' または 'cuda'（GPU使用の場合）
     name='yolov8n_finetuned',  # 結果保存フォルダ名
-    pretrained=True,  # 事前学習済みモデルを活用
-    patience = 25
+    pretrained='/home/shingo/find-it/sandbox/YOLOtest/runs/detect/yolov8n_finetuned5/weights/last.pt',  # 事前学習済みモデルを活用
 )
 
 # 追加学習したモデルを保存
